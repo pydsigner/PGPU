f1a217b22758a51b869ea53d7207e1d72151983b
Daniel Foerster <pydsigner@gmail.com>
2012-03-26 10:34:09 -0500

    Removed deprecated code from __init__.py

--- pgpu/__init__.py
+++ pgpu/__init__.py
@@ -18,34 +18,9 @@ tkinter2x       --  2.x and 3.x compatibility layer for tkinter
 
 Version information.
 
-NOTE: This module used to contain "Generic iterable utilities". This is now 
-deprecated, with the corresponding functions moved to iter_utils. The 
-compatibility layer in this module will be removed, perhaps in the next 
-release.
-
 AUTHORS:
 v0.2.0+             --> pydsigner
 v1.0.0+             --> pydsigner
+v1.0.3+             --> pydsigner
 '''
-__version__ = '1.0.2'
-
-import importlib
-import functools
-
-_warning = DeprecationWarning(
-        'This function has been moved to iter_utils, and is now deprecated!')
-
-
-def _runner(name, *args, **kw):
-    raise _warning
-    return getattr(importlib.import_module('iter_utils'), name)(*args, **kw)
-
-
-replace_many = functools.partial(_runner, 'replace_many')
-remove_many = functools.partial(_runner, 'remove_many')
-keep_many = functools.partial(_runner, 'keep_many')
-section = functools.partial(_runner, 'section')
-find = functools.partial(_runner, 'find')
-flatten = functools.partial(_runner, 'flatten')
-
-del _runner, importlib, functools
+__version__ = '1.0.3'
